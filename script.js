a128c6531f0519871302fc74b3be4bbeaf301c3c
Zach Bines <zachbines@gmail.com>
2022-12-03 18:37:50 -0500

    added hamburger menu on mobile

--- script.js
+++ script.js
@@ -20,3 +20,55 @@ scrollUpButton.addEventListener('click', () => {
   });
   
 })
+
+const hamburgerMenu = document.querySelector('.burger-menu');
+const nav = document.querySelector('.link-list');
+const body = document.querySelector('body');
+function burgerDropDown() {
+  if (!this.classList.contains('active')) {
+
+    [this, nav].forEach((elem) => {
+      elem.classList.add('active');
+    })
+    setTimeout(() => { nav.classList.add('blur')}, "100");
+    body.classList.add('noscroll');
+
+  } else {
+
+    [this, nav].forEach((elem) => {
+       setTimeout(() => { elem.classList.remove('active') }, "100");
+    })
+    nav.classList.remove('blur');
+    body.classList.remove('noscroll');
+  }
+}
+
+hamburgerMenu.addEventListener('click', burgerDropDown);
+
+
+
+const navLinks = document.querySelectorAll('.link-list .nav-link');
+
+
+function checkForMobile() {
+  let win = window.innerWidth;
+  if (win <= 575) {
+    navLinks.forEach((link) => {
+      link.addEventListener('click', revertNav);
+    })
+  } 
+}
+
+function revertNav() {
+  [hamburgerMenu, nav].forEach((elem) => {
+    setTimeout(() => {
+      elem.classList.remove('active');  
+    }, "500")
+
+  })
+  nav.classList.remove('blur');
+  body.classList.remove('noscroll');
+}
+
+window.onresize = checkForMobile;
+window.onload = checkForMobile;
